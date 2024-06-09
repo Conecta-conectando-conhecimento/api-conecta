a7141db2c07b95259eb947bc666fb6f4103cc46b
Riltter <riltterkennedy@gmail.com>
2024-06-09 16:17:20 -0300

    feat: ✨ add project files endpoint

---
+++ src/repositories/projectFiles.ts
@@ -0,0 +1,41 @@
+import { AppDataSource } from '../database/connection';
+import { ProjectFilesEntity } from '../entities/projectFiles';
+import { CreateProjectFileDTO, UpdateProjectFileDTO } from '../types/dto';
+
+const projectFilesRepository = AppDataSource.getRepository(ProjectFilesEntity);
+
+export class ProjectFilesRepository {
+    getAll = async (page?: number, limit?: number): Promise<ProjectFilesEntity[]> => {
+        const take = limit || 10;
+        const skip = (page && page > 0 ? page - 1 : 0) * take;
+
+        return await projectFilesRepository.find({
+            take,
+            skip,
+            order: {
+                saved_at: 'DESC',
+            },
+        });
+    };
+
+    getById = async (id: number): Promise<ProjectFilesEntity | undefined> => {
+        return await projectFilesRepository.findOne({ where: { id } });
+    };
+
+    getByProject = async (project_id: number): Promise<ProjectFilesEntity | undefined> => {
+        return await projectFilesRepository.findOne({where: { project_id }})
+    }
+
+    create = async (createProjectFileDTO: CreateProjectFileDTO): Promise<void> => {
+        const projectFile = projectFilesRepository.create(createProjectFileDTO);
+        await projectFilesRepository.save(projectFile);
+    };
+
+    update = async (id: number, updateProjectFileDTO: UpdateProjectFileDTO): Promise<void> => {
+        await projectFilesRepository.update({ id }, updateProjectFileDTO);
+    };
+
+    exclude = async (id: number): Promise<void> => {
+        await projectFilesRepository.delete({ id });
+    };
+}
---
+++ src/services/projectFiles.ts
@@ -0,0 +1,54 @@
+import { ProjectFilesRepository } from '../repositories/projectFiles';
+import { CreateProjectFileDTO, UpdateProjectFileDTO } from '../types/dto';
+
+const projectFilesRepository = new ProjectFilesRepository();
+
+export class ProjectFilesService {
+    async getAll(page?: number, limit?: number) {
+        const projectFiles = await projectFilesRepository.getAll(page, limit);
+        return { codehttp: 200, projectFiles };
+    }
+
+    async getById(id: number) {
+        const projectFile = await projectFilesRepository.getById(id);
+        if (projectFile) {
+            return { codehttp: 200, projectFile };
+        } else {
+            return { codehttp: 404, message: 'Project file not found' };
+        }
+    }
+
+    async getByProject(project_id: number) {
+        const projectFile = await projectFilesRepository.getByProject(project_id);
+        if (projectFile) {
+            return { codehttp: 200, projectFile };
+        } else {
+            return { codehttp: 404, message: 'Project file not found' };
+        }
+    }
+
+    async create(createProjectFileDTO: CreateProjectFileDTO) {
+        await projectFilesRepository.create(createProjectFileDTO);
+        return { codehttp: 201, message: 'Project file created successfully' };
+    }
+
+    async update(id: number, updateProjectFileDTO: UpdateProjectFileDTO) {
+        const existingProjectFile = await projectFilesRepository.getById(id);
+        if (existingProjectFile) {
+            await projectFilesRepository.update(id, updateProjectFileDTO);
+            return { codehttp: 200, message: 'Project file updated successfully' };
+        } else {
+            return { codehttp: 404, message: 'Project file not found' };
+        }
+    }
+
+    async exclude(id: number) {
+        const existingProjectFile = await projectFilesRepository.getById(id);
+        if (existingProjectFile) {
+            await projectFilesRepository.exclude(id);
+            return { codehttp: 200, message: 'Project file deleted successfully' };
+        } else {
+            return { codehttp: 404, message: 'Project file not found' };
+        }
+    }
+}
