b50c28ca1eeb89eb53ad163075fc058a586de553
Riltter <riltterkennedy@gmail.com>
2024-06-21 18:35:13 -0300

    refactor: ♻️ adicionando dependência para manipulação de arquivo e ajustando service de projectFiles para envio e exclusão correta de arquivos

--- src/services/projectFiles.ts
+++ src/services/projectFiles.ts
@@ -1,6 +1,8 @@
 import { ProjectFilesRepository } from '../repositories/projectFiles';
 import { CreateProjectFileDTO, UpdateProjectFileDTO } from '../types/dto';
 import supabase from '../config/utils/supabase_cliente';
+import path from 'path';
+import mime from 'mime-types';
 
 
 const projectFilesRepository = new ProjectFilesRepository();
@@ -31,34 +33,41 @@ export class ProjectFilesService {
 
     async create(createProjectFileDTO: CreateProjectFileDTO, file: Express.Multer.File) {
 
-        var file_url = `${file.originalname}-${Date.now()}`;
-        try {
-            const { data, error } = await supabase.storage
-                .from('projectfiles')
-                .upload(`files/${file_url}`, file.buffer, {
-                    cacheControl: '3600',
-                    upsert: false,
-                });
-            if (error) {
-                console.error('Error uploading file to Supabase:', error); 
-                throw new Error(`Failed to upload file to Supabase: ${error.message}`);
-            }
-    
-            createProjectFileDTO.file_url = `${process.env.SUPABASE_URL}/storage/v1/object/public/${data.fullPath}`;
-            createProjectFileDTO.small_file_url = `${data.path}`
-    
-            await projectFilesRepository.create(createProjectFileDTO);
-            return { codehttp: 201, message: 'Project file created successfully', fileUrl: data.path };
-        } catch (error) {
-            console.error('Error creating project file:', error);
-    
-            // Retornando uma mensagem mais específica em caso de falha
-            if (error.response && error.response.data && error.response.data.error) {
-                throw new Error(`Failed to create project file: ${error.response.data.error.message}`);
-            } else {
-                throw new Error('Unexpected error occurred while creating project file');
-            }
+       // Obter a extensão do arquivo
+    const fileExtension = path.extname(file.originalname);
+    // Construir a URL do arquivo com a extensão
+    const file_url = `${Date.now()}-${file.originalname}`;
+    // Detectar o tipo MIME
+    const mimeType = mime.lookup(fileExtension) || 'application/octet-stream';
+
+    try {
+        const { data, error } = await supabase.storage
+            .from('projectfiles')
+            .upload(`files/${file_url}`, file.buffer, {
+                cacheControl: '3600',
+                upsert: false,
+                contentType: mimeType,  // Definindo o tipo MIME correto
+            });
+        if (error) {
+            console.error('Error uploading file to Supabase:', error); 
+            throw new Error(`Failed to upload file to Supabase: ${error.message}`);
         }
+
+        createProjectFileDTO.file_url = `${process.env.SUPABASE_URL}/storage/v1/object/public/projectfiles/files/${file_url}`;
+        createProjectFileDTO.small_file_url = `/files/${file_url}`;
+
+        await projectFilesRepository.create(createProjectFileDTO);
+        return { codehttp: 201, message: 'Project file created successfully', fileUrl: data.path };
+    } catch (error) {
+        console.error('Error creating project file:', error);
+
+        // Retornando uma mensagem mais específica em caso de falha
+        if (error.response && error.response.data && error.response.data.error) {
+            throw new Error(`Failed to create project file: ${error.response.data.error.message}`);
+        } else {
+            throw new Error('Unexpected error occurred while creating project file');
+        }
+    }
     }
 
     async update(id: number, updateProjectFileDTO: UpdateProjectFileDTO) {
@@ -78,13 +87,14 @@ export class ProjectFilesService {
             return { codehttp: 404, message: 'Project file not found' };
         }
     
-        const filePath = existingProjectFile.small_file_url; 
+        const filePath = existingProjectFile.small_file_url;
+        const newFilePath = filePath.slice(-(filePath.length - 1)); // tirando uma /
     
-        console.log(`Tentando deletar o arquivo no caminho: ${filePath}`);
+        console.log(`Tentando deletar o arquivo no caminho: ${newFilePath}`);
     
         const { error: deleteError } = await supabase.storage
             .from('projectfiles')
-            .remove([filePath]);
+            .remove([newFilePath]);
     
         if (deleteError) {
             console.error('Erro ao deletar o arquivo do Supabase:', deleteError);
