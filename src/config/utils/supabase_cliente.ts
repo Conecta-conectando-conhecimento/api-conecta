d2d3b58bb9db2adbb07c5175b1c652f6d3aa91d9
Ana Luisa <anabonjardimbarros@gmail.com>
2024-06-24 14:42:51 -0300

    cofigbanco

--- src/config/utils/supabase_cliente.ts
+++ src/config/utils/supabase_cliente.ts
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabaseUrl = 'https://xhnrrtnynnrvpduxhkbp.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhobnJydG55bm5ydnBkdXhoa2JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAyODUyMTQsImV4cCI6MjAyNTg2MTIxNH0.8nNbeqg7YHvS6IBMDQ0saIlmeNWPj-NIe1HDrbmxqfs';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
