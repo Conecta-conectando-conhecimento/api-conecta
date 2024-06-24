6aef846742cbdac1fdc1598501f7623f7b5040da
Leonardo <lramoslima97@gmail.com>
2024-06-24 18:54:27 -0300

    refactor: :truck: change path o entities

---
+++ src/entities/index.ts
@@ -0,0 +1,15 @@
+import { MySavedEntity } from "./favorites";
+import { ParticipantsEntity } from "./participants";
+import { ParticipantViewEntity } from "./participantView";
+import { ProjectEntity } from "./project";
+import { ProjectFilesEntity } from "./projectFiles";
+import { UserEntity } from "./user";
+
+export default [
+    UserEntity,
+    MySavedEntity,
+    ParticipantsEntity,
+    ParticipantViewEntity,
+    ProjectEntity,
+    ProjectFilesEntity,
+]
