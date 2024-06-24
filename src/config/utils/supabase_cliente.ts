82ec8443442c26ab2e993f3b3b07854ea335e1b7
Ana Luisa <anabonjardimbarros@gmail.com>
2024-06-24 14:47:02 -0300

    configbancoainda

--- src/config/utils/supabase_cliente.ts
+++ src/config/utils/supabase_cliente.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xhnrrtnynnrvpduxhkbp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhobnJydG55bm5ydnBkdXhoa2JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAyODUyMTQsImV4cCI6MjAyNTg2MTIxNH0.8nNbeqg7YHvS6IBMDQ0saIlmeNWPj-NIe1HDrbmxqfs';
-
+const supabaseUrl = 'https://xhnrrtnynnrvpduxhkbp.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhobnJydG55bm5ydnBkdXhoa2JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAyODUyMTQsImV4cCI6MjAyNTg2MTIxNH0.8nNbeqg7YHvS6IBMDQ0saIlmeNWPj-NIe1HDrbmxqfs'
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
