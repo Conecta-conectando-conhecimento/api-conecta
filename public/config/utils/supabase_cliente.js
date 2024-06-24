f336882e93957e36f275f1332d72857f1fea093c
Ana Luisa <anabonjardimbarros@gmail.com>
2024-06-24 14:49:07 -0300

    esquecidojs

--- public/config/utils/supabase_cliente.js
+++ public/config/utils/supabase_cliente.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const supabase_js_1 = require("@supabase/supabase-js");
-const supabase = (0, supabase_js_1.createClient)(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabaseUrl = 'https://xhnrrtnynnrvpduxhkbp.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhobnJydG55bm5ydnBkdXhoa2JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAyODUyMTQsImV4cCI6MjAyNTg2MTIxNH0.8nNbeqg7YHvS6IBMDQ0saIlmeNWPj-NIe1HDrbmxqfs';
+const supabase = (0, supabase_js_1.createClient)(supabaseUrl, supabaseKey);
 exports.default = supabase;
